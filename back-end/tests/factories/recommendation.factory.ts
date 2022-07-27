b5474a23d1ae483db7fbe746685bd6f2d1979f3f
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-27 01:23:35 -0300

    feat: create many recommendatios on factory

--- back-end/tests/factories/recommendation.factory.ts
+++ back-end/tests/factories/recommendation.factory.ts
@@ -1,10 +1,24 @@
 import { Recommendation } from '@prisma/client';
+import { faker } from '@faker-js/faker';
 import { prisma } from '../../src/database.js';
 import { recommendationBodyFactory } from './recommendationBody.factory';
 
 export const createRecommendation = async (): Promise<Recommendation> => {
+  const body = recommendationBodyFactory();
   const recommendation = await prisma.recommendation.create({
-    data: recommendationBodyFactory(),
+    data: {
+      ...body,
+      score: faker.datatype.number({ min: 0, max: 50 }),
+    },
   });
   return recommendation;
+};
+
+export const createManyRecommendations = async (
+  count: number,
+): Promise<Recommendation[]> => {
+  const recommendations = await Promise.all(
+    Array.from({ length: count }, () => createRecommendation()),
+  );
+  return recommendations;
 };
